d4b162a6b699bfff71c1ef603faeaf750d6b7134
Tiffany <tgateley22@gmail.com>
2024-08-15 02:40:42 -0500

    Day 28: Add links, specify language, add subject line, control topic

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import requests
 from send_email import send_email
 
+topic = "techcrunch"
+
 api_key = "f35f9b1357fa499f892171e58c36c193"
-url = ("https://newsapi.org/v2/top-headlines?sources="
-       "techcrunch&apiKey=f35f9b1357fa499f892171e58c36c193")
+url = ("https://newsapi.org/v2/top-headlines?"
+       f"sources={topic}&"
+       "apiKey=f35f9b1357fa499f892171e58c36c193&"
+       "language=en")
 
 # Make a request
 request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
-email = " "
+email = "Subject: Today's News" + 2*"\n"
 # Access the article titles and description
-for article in content["articles"]:
+for article in content["articles"][:5]:
     if article["title"] is not None:
-        email = email + article["title"] + "\n" + article["description"] + 2*"\n"
+        email = (email + article["title"] + 2*"\n"
+                 + article["description"] +
+                 "\n" + article["url"] + 3*"\n")
 
 email = email.encode("utf-8")
 send_email(message=email)
