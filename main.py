56b495e879c76a0e240c2a825c28304d933e0d18
Tiffany <tgateley22@gmail.com>
2024-08-15 02:17:27 -0500

    Day 27: Send email with news titles and description

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from send_email import send_email
+
 api_key = "f35f9b1357fa499f892171e58c36c193"
 url = ("https://newsapi.org/v2/top-headlines?sources="
        "techcrunch&apiKey=f35f9b1357fa499f892171e58c36c193")
@@ -8,8 +10,11 @@ request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
-
-# Access the article titles and descriotion
+email = " "
+# Access the article titles and description
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        email = email + article["title"] + "\n" + article["description"] + 2*"\n"
+
+email = email.encode("utf-8")
+send_email(message=email)
